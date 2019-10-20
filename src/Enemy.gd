ba292072406631b0132c0aab2cd3ea2778ad082e
Cameron Reikes <cameronreikes@gmail.com>
2019-10-19 21:26:38 -0700

    Make it so enemies don't move

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -9,5 +9,5 @@ func _physics_process(delta):
 	if player_node.global_position.distance_to(global_position) >= despawn_distance:
 		emit_signal("dead")
 		queue_free()
-	applied_force = (player_node.global_position - global_position).normalized()*800.0
+#	applied_force = (player_node.global_position - global_position).normalized()*800.0
 #	move_and_slide((player_node.global_position - global_position).normalized()*600.0)
