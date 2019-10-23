328da8a3c9b05c5bb3b0dbb45c903cf1c9ebdb9f
Cameron Reikes <cameronreikes@gmail.com>
2019-10-22 21:18:32 -0700

    Tune spawning and player values

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,9 +1,27 @@
+tool
 extends RigidBody2D
 
 signal dead
 
+export var size: int = 30 setget set_size
+
 var despawn_distance: float = 500.0
 var player_node: Node2D
+
+func set_size(new_size):
+	if new_size % 2 != 0:
+		new_size -= sign(size - new_size)
+	size = new_size
+	if has_node("ColorRect") and has_node("CollisionShape2D"):
+		var size_vector = Vector2(size, size)
+		$ColorRect.rect_size = size_vector
+		$ColorRect.rect_position = -size_vector/2
+		$CollisionShape2D.shape.extents = size_vector/2
+
+func _ready():
+	if Engine.editor_hint:
+		set_physics_process(false)
+		set_process(false)
 
 func _physics_process(delta):
 	if player_node.global_position.distance_to(global_position) >= despawn_distance:
--- src/PlayerTurning.gd
+++ src/PlayerTurning.gd
@@ -1,29 +1,13 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export var forward_force = 2000
-export var frictional_force = 10000
-export var turning_torque = 400.0
-export var dash_impulse = 5000.0
-
+export var movement_speed = 700.0
+export var rotational_speed = 360.0
 
 func _physics_process(delta):
 	var horizontal: int = int(Input.is_action_pressed("g_right")) - int(Input.is_action_pressed("g_left"))
 	var vertical: int = int(Input.is_action_pressed("g_up")) - int(Input.is_action_pressed("g_down"))
-	var dash: int = int(Input.is_action_just_pressed("g_dash_right")) - int(Input.is_action_just_pressed("g_dash_left"))
-	applied_force = Vector2()
+#	var dash: int = int(Input.is_action_just_pressed("g_dash_right")) - int(Input.is_action_just_pressed("g_dash_left"))
 	
-	# Move forward constantly
-	applied_force += Vector2(0, -forward_force).rotated(rotation)
+	rotation += float(horizontal)*deg2rad(rotational_speed)*delta
 	
-	# Dampeners
-	var dampening_force = frictional_force*(-linear_velocity).normalized()
-	# Remove dampening in direction of travel
-	var dampening_facing = abs(min(0, dampening_force.normalized().dot(applied_force.normalized())))*-1 + 1
-	applied_force += dampening_force*dampening_facing
-	
-	# Turn from input
-	angular_velocity = horizontal*deg2rad(turning_torque)
-	
-	# Apply dashing
-	if dash != 0:
-		apply_central_impulse(Vector2(float(dash)*dash_impulse, 0).rotated(rotation))
+	move_and_collide(Vector2(0, -movement_speed).rotated(rotation)*delta, false)
