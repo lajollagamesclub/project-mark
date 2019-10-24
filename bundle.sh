8f679c95ba75c4517695974bfac92b65a0e187e4
Cameron Reikes <cameronreikes@gmail.com>
2019-10-23 21:33:41 -0700

    Add windows and linux export

--- bundle.sh
+++ bundle.sh
@@ -4,12 +4,17 @@ if [ ! -f "tmp.tres" ]; then # avoid overriding previously cached tmp ( after fa
 	cp /home/creikey/.config/godot/editor_settings-3.tres tmp.tres
 fi
 
+rm -r exports
 mkdir -p exports/web
-rm -r exports/web
-mkdir exports/web
+mkdir -p exports/linux
+mkdir -p exports/windows
 godot-headless --path src --export web "$(readlink -f ./exports/web/index.html)"
+godot-headless --path src --export windows "$(readlink -f ./exports/windows/project-mark.exe)"
+godot-headless --path src --export linux "$(readlink -f ./exports/linux/project-mark.x86_64)"
 if [ "$?" == "0" ]; then
 	butler push exports/web lajollagamesclub/project-mark:web 
+	butler push exports/windows lajollagamesclub/project-mark:windows
+	butler push exports/linux lajollagamesclub/project-mark:linux
 fi
 
 mv tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
