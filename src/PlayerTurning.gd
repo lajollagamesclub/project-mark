1eaba5271d2d6b2e02c7e5c521cdc40332ff04a8
Cameron Reikes <cameronreikes@gmail.com>
2019-10-23 20:50:02 -0700

    Punish player for failing to avoid asteroids

--- src/PlayerTurning.gd
+++ src/PlayerTurning.gd
@@ -11,18 +11,20 @@ func _process(delta):
 		if tapped and not is_on_wall():
 			var asteroid_distance: float = global_position.distance_to(asteroid.global_position)/$AsteroidVisualizations/CollisionShape2D.shape.radius
 			player_state.fuel += lerp(0.0, 10.0, asteroid_distance)*delta
+	if is_on_wall():
+		player_state.fuel -= 100.0*delta
 
 func _physics_process(delta):
 	var horizontal: int = int(Input.is_action_pressed("g_right")) - int(Input.is_action_pressed("g_left"))
 	var vertical: int = int(Input.is_action_pressed("g_up")) - int(Input.is_action_pressed("g_down"))
 #	var dash: int = int(Input.is_action_just_pressed("g_dash_right")) - int(Input.is_action_just_pressed("g_dash_left"))
 	
-	if Input.is_action_just_pressed("g_fire") and player_state.fuel > 0:
+	if Input.is_action_just_pressed("g_fire") and player_state.can_fire_bullet():
 		var cur_bullet = preload("res://Bullet.tscn").instance()
 		get_parent().get_node("Bullets").add_child(cur_bullet)
 		cur_bullet.global_position = global_position
 		cur_bullet.rotation = global_position.angle_to_point(get_global_mouse_position()) + PI
-		preload("res://player_state.tres").fire_bullet()
+		player_state.fire_bullet()
 	
 	rotation += float(horizontal)*deg2rad(rotational_speed)*delta
 	# Vector2 move_and_slide( Vector2 linear_velocity, Vector2 floor_normal=Vector2( 0, 0 ), bool stop_on_slope=false, int max_slides=4, float floor_max_angle=0.785398, bool infinite_inertia=true )
--- src/player_state.gd
+++ src/player_state.gd
@@ -5,10 +5,13 @@ signal fuel_changed(new_fuel)
 var fuel: float = 100 setget set_fuel
 
 func set_fuel(new_fuel):
-	if new_fuel > 100:
+	if new_fuel > 100 or new_fuel < 0:
 		return
 	fuel = new_fuel
 	emit_signal("fuel_changed", fuel)
+
+func can_fire_bullet():
+	return fuel > 5
 
 func fire_bullet():
 	self.fuel -= 5
