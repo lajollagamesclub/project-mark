dbee42ed20cfb08b7af8e3d066e76a2d7b41c733
Cameron Reikes <cameronreikes@gmail.com>
2019-10-25 12:35:03 -0700

    Trim useless decimal info

--- src/CurTimeLabel.gd
+++ src/CurTimeLabel.gd
@@ -3,4 +3,4 @@ extends Label
 const game_state = preload("res://game_state.tres")
 
 func _process(delta):
-	text = str("Current Time: ", game_state.time)
+	text = str("Current Time: ", stepify(game_state.time, 0.01))
--- src/TimeLabel.gd
+++ src/TimeLabel.gd
@@ -3,4 +3,4 @@ extends Label
 const game_state = preload("res://game_state.tres")
 
 func _ready():
-	text = str("Time: ", game_state.time)
+	text = str("Time: ", stepify(game_state.time, 0.01))
