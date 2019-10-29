054bfede106712909afd67c075f2a7d80b8c79bc
Cameron Reikes <cameronreikes@gmail.com>
2019-10-29 12:38:50 -0700

    Add smartly switching universe for performance

--- src/Main.gd
+++ src/Main.gd
@@ -4,3 +4,7 @@ const game_state = preload("res://game_state.tres")
 
 func _process(delta):
 	game_state.time += delta
+
+func _input(event):
+	if event.is_action_pressed("g_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
--- src/StarsPerformanceSwitcher.gd
+++ src/StarsPerformanceSwitcher.gd
@@ -1,9 +1,34 @@
 extends Node2D
 
+var is_high_performance = true setget set_is_high_performance
+
+var fps_measurements: int = 0
+var cur_fps_average: float = 0.0
+var cur_fps_measuring_counter: float = 0
+
 func _ready():
-	if OS.get_name() == "Android":
-		$HighPerformanceStars.visible = false
-		$LowPerformanceStars.visible = true
-	else:
-		$HighPerformanceStars.visible = true
-		$LowPerformanceStars.visible = false
+	self.is_high_performance = true
+	set_process(false)
+
+func _process(delta):
+	cur_fps_measuring_counter += delta
+	fps_measurements += 1
+	cur_fps_average += Engine.get_frames_per_second()
+	print(Engine.get_frames_per_second(), "	", cur_fps_average, "	", fps_measurements)
+#	print(cur_fps_average/float(fps_measurements))
+	if cur_fps_measuring_counter >= 1.0:
+		
+		if cur_fps_average/float(fps_measurements) <= 40.0:
+			self.is_high_performance = false
+		else:
+			self.is_high_performance = true
+		set_process(false)
+
+func set_is_high_performance(new_is_high_performance):
+	is_high_performance = new_is_high_performance
+	$HighPerformanceStars.visible = is_high_performance
+	$LowPerformanceStars.visible = !is_high_performance
+
+
+func _on_StartMeasuringTimer_timeout():
+	set_process(true)
