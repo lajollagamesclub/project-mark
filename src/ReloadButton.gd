fe12c43ce758295c02445ad31f55eadf88119509
Cameron Reikes <cameronreikes@gmail.com>
2019-10-30 18:22:23 -0700

    Press space to restart

--- src/ReloadButton.gd
+++ src/ReloadButton.gd
@@ -2,6 +2,13 @@ extends Button
 
 const game_state = preload("res://game_state.tres")
 
+func _input(event):
+	if event.is_action_pressed("g_reset"):
+		reload()
+
 func _on_ReloadButton_pressed():
+	reload()
+
+func reload():
 	game_state.time = 0.0
 	get_tree().change_scene("res://Main.tscn")
