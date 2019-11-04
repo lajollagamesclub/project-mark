a7ea42d12f77c432f90241e3da07d260474e93d5
Cameron Reikes <cameronreikes@gmail.com>
2019-11-04 00:48:03 -0800

    Increase number of asteroids with difficulty

--- src/AsteroidSpawner.gd
+++ src/AsteroidSpawner.gd
@@ -1,18 +1,24 @@
 tool
 extends Node2D
 
+const game_state = preload("res://game_state.tres")
+
 export (bool) var show_spawn_visually = false
 export var safe_radius = 500.0
 export var camera_view_radius = 400.0
 export var player_visibility_radius = 1000.0
-export var target_enemies: int = 60
+#export var target_enemies: int = 60
 export (PackedScene) var to_spawn_pack
 export (NodePath) var player_path = ""
+export (Curve) var spawn_time_curve: Curve
+export (float) var max_distance: float = 50000 # will stop scaling difficulty
 
+var target_enemies: int = 60
 var player_node: Node2D = null
 var number_of_enemies: int = 0
 
 func _ready():
+	spawn_time_curve.bake()
 	if !Engine.editor_hint:
 		player_node = get_node(player_path)
 	randomize()
@@ -36,6 +42,9 @@ func _process(delta):
 			cur_enemy.connect("dead", self, "enemy_died")
 			add_child(cur_enemy)
 			number_of_enemies += 1
+
+	var cur_distance_fraction: float = min(1.0, float(game_state.cur_distance)/max_distance)
+	target_enemies = spawn_time_curve.interpolate_baked(cur_distance_fraction)
 
 func enemy_died():
 	number_of_enemies -= 1
