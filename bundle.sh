1b070f943530abc83e75c6aa2757deb4c7e8a00e
Cameron Reikes <cameronreikes@gmail.com>
2019-11-03 21:47:49 -0800

    Add mac platform to bundle

--- bundle.sh
+++ bundle.sh
@@ -8,13 +8,20 @@ rm -r exports
 mkdir -p exports/web
 mkdir -p exports/linux
 mkdir -p exports/windows
+mkdir -p exports/mac
 godot-headless --path src --export web "$(readlink -f ./exports/web/index.html)"
 godot-headless --path src --export windows "$(readlink -f ./exports/windows/project-mark.exe)"
 godot-headless --path src --export linux "$(readlink -f ./exports/linux/project-mark.x86_64)"
+godot-headless --path src --export mac "$(readlink -f ./exports/mac/Project\ Mark)"
 if [ "$?" == "0" ]; then
 	butler push exports/web lajollagamesclub/project-mark:web 
 	butler push exports/windows lajollagamesclub/project-mark:windows
 	butler push exports/linux lajollagamesclub/project-mark:linux
+
+	cd exports/mac
+	mv Project\ Mark Project\ Mark.zip
+	unzip "Project Mark.zip"
+	butler push Project\ Mark.app lajollagamesclub/project-mark:mac 
 fi
 
 mv tmp.tres /home/creikey/.config/godot/editor_settings-3.tres
